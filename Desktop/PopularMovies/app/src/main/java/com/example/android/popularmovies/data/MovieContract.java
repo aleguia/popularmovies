63758155f438d55d9be93bc4304df3e26579873a
Fernando Leguia <aleguia88@gmail.com>
2016-11-15 17:20:33 -0500

    Add MovieContract, MovieDbHelper and MovieProvider

---
+++ Desktop/PopularMovies/app/src/main/java/com/example/android/popularmovies/data/MovieContract.java
@@ -0,0 +1,55 @@
+package com.example.android.popularmovies.data;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Fernando on 14/11/2016.
+ */
+
+public class MovieContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.popularmovies.app";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_MOVIE = "movie";
+
+
+    /* Inner class that defines the table contents of the movie table */
+    public static final class MovieEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+        //Table name
+        public static final String TABLE_NAME = "movie";
+
+        //The id column for movie table provided by theopenmoviedb
+
+        public static final String MOVIE_ID = "movie_id";
+
+        public static final String COLUMN_TITLE ="title";
+
+        public static final String COLUMN_DATE = "release_date";
+
+        public static final String COLUMN_POSTER = "poster_path";
+
+        public static final String COLUMN_RATE = "vote_average";
+
+        //the order by setting that´s going to be sent to the api
+        public static final String ORDER_BY = "orderby";
+
+        public static Uri buildMovieUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI,id);
+        }
+
+
+    }
+}
---
+++ Desktop/PopularMovies/app/src/main/java/com/example/android/popularmovies/data/MovieDbHelper.java
@@ -0,0 +1,41 @@
+package com.example.android.popularmovies.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Fernando on 15/11/2016.
+ */
+
+public class MovieDbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 2;
+
+    static final String DATABASE_NAME = "movie.db";
+
+    public MovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        final String    SQL_CREATE_MOVIE_TABLE = "CREATE TABLE" + MovieContract.MovieEntry.TABLE_NAME + "(" +
+                MovieContract.MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                MovieContract.MovieEntry.MOVIE_ID + "INTEGER NOT NULL," +
+                MovieContract.MovieEntry.COLUMN_TITLE + "TEXT UNIQUE NOT NULL," +
+                MovieContract.MovieEntry.COLUMN_DATE + "INTEGER NOT NULL,"+
+                MovieContract.MovieEntry.COLUMN_POSTER + "TEXT NOT NULL"+
+                MovieContract.MovieEntry.COLUMN_RATE + "REAL NOT NULL"+")";
+
+        db.execSQL(SQL_CREATE_MOVIE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP IF TABLE EXITS " + MovieContract.MovieEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
