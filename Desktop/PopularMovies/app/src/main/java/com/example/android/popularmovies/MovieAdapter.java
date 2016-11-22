9141c2eb77acf77d5e4bdc5cc5c4d0ee6a06aa61
Fernando Leguia <aleguia88@gmail.com>
2016-11-22 16:29:57 -0500

    Working fine for movies sorted by popular

--- Desktop/PopularMovies/app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
+++ Desktop/PopularMovies/app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.popularmovies;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,8 @@ import static com.squareup.picasso.Picasso.with;
 
 public class MovieAdapter extends CursorAdapter {
 
+    private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
+
     public MovieAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
@@ -34,10 +37,12 @@ public class MovieAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
 
         ImageView imageView = (ImageView) view.findViewById(R.id.movie_imageview);
-        int id_path = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_POSTER);
+
+        String path = cursor.getString(MovieFragment.COL_MOVIE_POSTER);
+//        Log.v(LOG_TAG, path);
         Picasso
                 .with(context)
-                .load("http://image.tmdb.org/t/p/w185/" + cursor.getString(id_path))
+                .load("http://image.tmdb.org/t/p/w185/" + path)
                 .into(imageView);
 
     }
--- Desktop/PopularMovies/app/src/main/java/com/example/android/popularmovies/data/MovieContract.java
+++ Desktop/PopularMovies/app/src/main/java/com/example/android/popularmovies/data/MovieContract.java
@@ -5,6 +5,8 @@ import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
+import static android.R.attr.id;
+
 /**
  * Created by Fernando on 14/11/2016.
  */
@@ -16,6 +18,8 @@ public class MovieContract {
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     public static final String PATH_MOVIE = "movie";
+
+
 
 
     /* Inner class that defines the table contents of the movie table */
@@ -48,6 +52,46 @@ public class MovieContract {
 
         public static Uri buildMovieUri(long id){
             return ContentUris.withAppendedId(CONTENT_URI,id);
+        }
+
+        public static Uri buildMovieWithIdUri(long id){
+            return CONTENT_URI.buildUpon().appendPath(Long.toString(id)).build();
+        }
+    }
+
+    public static final class FavouriteMovieEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+        //Table name
+        public static final String TABLE_NAME = "movie";
+
+        //The id column for movie table provided by theopenmoviedb
+
+        public static final String MOVIE_ID = "movie_id";
+
+        public static final String COLUMN_TITLE ="title";
+
+        public static final String COLUMN_DATE = "release_date";
+
+        public static final String COLUMN_POSTER = "poster_path";
+
+        public static final String COLUMN_RATE = "vote_average";
+
+        //the order by setting that´s going to be sent to the api
+        public static final String ORDER_BY = "orderby";
+
+        public static Uri buildMovieUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI,id);
+        }
+
+        public static Uri buildMovieWithIdUri(long id){
+            return CONTENT_URI.buildUpon().appendPath(Long.toString(id)).build();
         }
     }
 }
