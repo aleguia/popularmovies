66ddb958366d270c741c1e0b68664dafd81cf2ef
Fernando Leguia <aleguia88@gmail.com>
2016-11-23 11:03:51 -0500

    prueba

--- Desktop/PopularMovies/app/src/main/java/com/example/android/popularmovies/UpdateMovieAsFavourite.java
+++ Desktop/PopularMovies/app/src/main/java/com/example/android/popularmovies/UpdateMovieAsFavourite.java
@@ -1,17 +1,43 @@
 package com.example.android.popularmovies;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.AsyncTask;
+import android.support.v4.app.LoaderManager;
+
+import com.example.android.popularmovies.data.MovieContract;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+import static java.security.AccessController.getContext;
 
 /**
  * Created by Fernando on 22/11/2016.
  */
 
-public class UpdateMovieAsFavourite extends AsyncTask<Void,Void,Void> {
+public class UpdateMovieAsFavourite extends AsyncTask<Void,Void,Void>  {
+
+
+
+
+
 
     @Override
     protected Void doInBackground(Void... params) {
 
+        deleteOrSaveFovouriteMovie();
+
     return null;}
+
+    private void deleteOrSaveFovouriteMovie(){
+
+
+
+        Cursor cursor = getContext().getContentResolver().query(MovieContract.FavouriteMovieEntry.CONTENT_URI,
+                        new String[]{MovieContract.FavouriteMovieEntry.MOVIE_ID},
+                        MovieContract.FavouriteMovieEntry.MOVIE_ID + " = ?",
+                        new String[]{String.valueOf()}
+                )
+
+    }
 }
